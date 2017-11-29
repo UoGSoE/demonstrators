0932e8f46014ed1daeb38cd3775a8da539c907a0
Finlay Maciver <finlay.maciver@glasgow.ac.uk>
2017-11-29 11:42:46 +0000

    Gitlab CI

--- gitlab-ci.sh
+++ gitlab-ci.sh
@@ -12,7 +12,7 @@ apt-get install -y libxml2-dev --no-install-recommends
 rm -rf /var/lib/apt/lists/*
 
 # Compile PHP, include these extensions.
-docker-php-ext-install pdo_mysql zip bcmath process
+docker-php-ext-install pdo_mysql zip bcmath posix
 docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/
 docker-php-ext-install ldap
 
