080e4c45bdb857672ecd42d6441e382660f2cfd2
Finlay Maciver <finlay.maciver@glasgow.ac.uk>
2017-12-04 10:18:11 +0000

    Added download button

--- resources/views/admin/reports/output3.blade.php
+++ resources/views/admin/reports/output3.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <div class="columns is-centered">
   <div class="column is-three-quarters">
-    <h3 class="title is-3">Accepted Students</h3>
+    <h3 class="title is-3">Accepted Students
+      <a class="button is-gla is-outlined is-pulled-right" href="{{route('admin.reports.output3.download')}}">
+        <span class="icon is-small">
+          <i class="fa fa-download" aria-hidden="true"></i>
+        </span>
+      </a>
+    </h3>
   </div>
 </div>
 <div class="columns is-centered">
