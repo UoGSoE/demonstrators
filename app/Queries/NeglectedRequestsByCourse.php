75f28cd275cfb23629d28dca9a37f634a4dfb002
Finlay Maciver <finlay.maciver@glasgow.ac.uk>
2017-12-08 10:37:57 +0000

    Extra fix for output 6

--- app/Queries/NeglectedRequestsByCourse.php
+++ app/Queries/NeglectedRequestsByCourse.php
@@ -13,7 +13,7 @@ class NeglectedRequestsByCourse
         return DemonstratorApplication::unaccepted()->unseen()
             ->where('created_at', '<', Carbon::now()->subdays(3))->with([
             'request.course' => function ($query) {
-                $query->groupBy('courses.id');
+                $query->groupBy('courses.id', 'courses.code');
             }
             ])->get()->unique('request_id');
     }
