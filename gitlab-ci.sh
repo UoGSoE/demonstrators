8024c2461075f69d6dd4bfa6b4d51ebdb7814b3d
Billy <william.allan@glasgow.ac.uk>
2018-01-03 09:32:07 +0000

    CI experiment

--- gitlab-ci.sh
+++ gitlab-ci.sh
@@ -6,27 +6,27 @@
 set -xe
 
 # Update packages and install composer and PHP dependencies.
-apt-get update -yqq
-apt-get install git zlib1g-dev libldap2-dev libpcre3-dev -yqq
-apt-get install -y libxml2-dev --no-install-recommends
-rm -rf /var/lib/apt/lists/*
+#apt-get update -yqq
+#apt-get install git zlib1g-dev libldap2-dev libpcre3-dev -yqq
+#apt-get install -y libxml2-dev --no-install-recommends
+#rm -rf /var/lib/apt/lists/*
 
 # Compile PHP, include these extensions.
-docker-php-ext-install pdo_mysql zip bcmath pcntl
-docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/
-docker-php-ext-install ldap
+#docker-php-ext-install pdo_mysql zip bcmath pcntl
+#docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/
+#docker-php-ext-install ldap
 
 # Install Composer and project dependencies.
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+#curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # Ping the mysql container
 # ping -c 3 mysql
 
 # Composer install parallel install plugin
-composer -q global require "hirak/prestissimo:^0.3"
+#composer -q global require "hirak/prestissimo:^0.3"
 
 # Install php code sniffer
-curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+#curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
 
 # Copy over testing configuration.
 cp -f .env.gitlab .env
