9288919612dff49aef98def28c40a43c38580b7b
finlay.maciver <finlay.maciver@glasgow.ac.uk>
2019-10-10 14:34:13 +0100

    Updated email for Sharon.

--- resources/views/emails/student/rtw_received.blade.php
+++ resources/views/emails/student/rtw_received.blade.php
@@ -5,7 +5,7 @@ Thank you for completing your Right to Work (RtW) check prior to working for the
 
 The process of creating your contract will now begin. Please note that this typically take 2-4 weeks from the date of your RtW check, and the same contract will cover this course and any others you have been selected for.
 
-Your contract will be sent to you by our HR department. Once received you must sign the contract, complete your payroll details and return these to the R721, Rankine Building. Following this, you will receive an email confirming that you may commence work. If you do not receive this email by the end of the next working day following submission of your signed contract, please contact the Teaching Office.
+Your contract will be sent to you by our HR department. Once received you must sign the contract, complete your payroll details and return these to R721, Rankine Building. Following this, you will receive an email confirming that you may commence work. If you do not receive this email by the end of the next working day following submission of your signed contract, please contact R721, Rankine Building.
 
 PLEASE NOTE THAT YOU MUST AWAIT RECEIPT OF YOUR CONFIRMATION EMAIL BEFORE COMMENCING WORK
 
