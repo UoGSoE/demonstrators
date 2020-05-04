ee7292a5547763703c9458cd9e3c10b90290e724
finlay.maciver <finlay.maciver@glasgow.ac.uk>
2020-05-04 10:12:02 +0100

    Updated npm packages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,6 +26,7 @@
     <meta name="theme-color" content="#ffffff ">
 
 </head>
+
 <body>
     <nav class="navbar">
         <div class="navbar-brand">
@@ -35,66 +37,66 @@
                 </p>
             </a>
             @if (Auth::user() and Auth::user()->is_admin)
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        <span>Admin</span>
-                    </a>
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item" href="{{ route('admin.edit_contracts') }}">
-                            <span>Students</span>
-                        </a>
-                        <a class="navbar-item" href="{{ route('admin.staff.index') }}">
-                            <span>Staff</span>
-                        </a>
-                        <a class="navbar-item" href="{{ route('admin.courses.index') }}">
-                            <span>Courses</span>
-                        </a>
-                        <a class="navbar-item" href="{{ route('admin.degreelevels.index') }}">
-                            <span>Degree Levels</span>
-                        </a>
-                        <a class="navbar-item" href="{{ route('admin.requests') }}">
-                            <span>Requests</span>
-                        </a>
-                        <a class="navbar-item" href="{{ route('admin.system.index') }}">
-                            <span>System</span>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        <span>Reports</span>
-                    </a>
-                    <div class="navbar-dropdown">
-                        <a href="{{route('admin.reports.output1')}}" class="navbar-item">Full Data Set</a>
-                        <a href="{{route('admin.reports.output2')}}" class="navbar-item">Applications (By Course)</a>
-                        <a href="{{route('admin.reports.output4')}}" class="navbar-item">Accepted Students (By Course)</a>
-                        <a href="{{route('admin.reports.output3')}}" class="navbar-item">Confirmed Students</a>
-                        <a href="{{route('admin.reports.output5')}}" class="navbar-item">Requests With No Applications</a>
-                        <a href="{{route('admin.reports.output6')}}" class="navbar-item">Unseen Applications Older Than 3 Days</a>
-                        <a href="{{route('admin.reports.output7')}}" class="navbar-item">Unaccepted Applications</a>
-                    </div>
-                </div>
-
-                <a class="navbar-item" href="{{ route('import.index') }}">
-                    <span>Import</span>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    <span>Admin</span>
                 </a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="{{ route('admin.edit_contracts') }}">
+                        <span>Students</span>
+                    </a>
+                    <a class="navbar-item" href="{{ route('admin.staff.index') }}">
+                        <span>Staff</span>
+                    </a>
+                    <a class="navbar-item" href="{{ route('admin.courses.index') }}">
+                        <span>Courses</span>
+                    </a>
+                    <a class="navbar-item" href="{{ route('admin.degreelevels.index') }}">
+                        <span>Degree Levels</span>
+                    </a>
+                    <a class="navbar-item" href="{{ route('admin.requests') }}">
+                        <span>Requests</span>
+                    </a>
+                    <a class="navbar-item" href="{{ route('admin.system.index') }}">
+                        <span>System</span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    <span>Reports</span>
+                </a>
+                <div class="navbar-dropdown">
+                    <a href="{{route('admin.reports.output1')}}" class="navbar-item">Full Data Set</a>
+                    <a href="{{route('admin.reports.output2')}}" class="navbar-item">Applications (By Course)</a>
+                    <a href="{{route('admin.reports.output4')}}" class="navbar-item">Accepted Students (By Course)</a>
+                    <a href="{{route('admin.reports.output3')}}" class="navbar-item">Confirmed Students</a>
+                    <a href="{{route('admin.reports.output5')}}" class="navbar-item">Requests With No Applications</a>
+                    <a href="{{route('admin.reports.output6')}}" class="navbar-item">Unseen Applications Older Than 3 Days</a>
+                    <a href="{{route('admin.reports.output7')}}" class="navbar-item">Unaccepted Applications</a>
+                </div>
+            </div>
+
+            <a class="navbar-item" href="{{ route('import.index') }}">
+                <span>Import</span>
+            </a>
             @endif
         </div>
         <div class="navbar-end">
             @if (Auth::guest())
-                <a class="navbar-item" href="/login">Login</a>
+            <a class="navbar-item" href="/login">Login</a>
             @elseif (session('original_id'))
-                    <form class="navbar-item" method="POST" action="{{route('admin.impersonate.stop')}}">
-                        {{ csrf_field() }}
-                        <input type="hidden" name="_method" value="DELETE">
-                        <button style="box-shadow:none;" class="button is-focus is-primary">Stop Impersonating {{ auth()->user()->full_name }}</button>
-                    </form>
+            <form class="navbar-item" method="POST" action="{{route('admin.impersonate.stop')}}">
+                {{ csrf_field() }}
+                <input type="hidden" name="_method" value="DELETE">
+                <button style="box-shadow:none;" class="button is-focus is-primary">Stop Impersonating {{ auth()->user()->full_name }}</button>
+            </form>
             @else
-                <form class="navbar-item" method="POST" action="/logout">
-                    {{ csrf_field() }}
-                    <button style="box-shadow:none;" class="button is-focus is-primary">Logout</button>
-                </form>
+            <form class="navbar-item" method="POST" action="/logout">
+                {{ csrf_field() }}
+                <button style="box-shadow:none;" class="button is-focus is-primary">Logout</button>
+            </form>
             @endif
         </div>
     </nav>
@@ -103,13 +105,14 @@
         <noscript>
             This site needs javascript enabled in order to work
         </noscript>
-    @yield('content')
+        @yield('content')
     </section>
     <!-- Scripts -->
     @stack('scripts')
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{!! asset('/js/datatables.min.js') !!}"></script>
     <script src="{{ asset('js/demonstrators.js') }}"></script>
     @yield('scripts')
 </body>
+
 </html>
